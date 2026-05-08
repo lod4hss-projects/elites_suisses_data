1f4e751bb29df24e483d0acad66068f3e70c9dde
Francesco Beretta <francesco.beretta@cnrs.fr>
2026-05-08 09:39:20 +0200

    test sparql on new graph

--- documentation/database_inspection/person_marriage.sql
+++ documentation/database_inspection/person_marriage.sql
@@ -16,6 +16,24 @@ limit 20;
 
 
 
+/*
+ * données sales qui font un probléme de URI
+ */
+SELECT *
+FROM elites_suisses.mariage m 
+where m."idMariage" like '51262104964%'
+limit 10;
+
+
+-- donnes sales corrigées
+update elites_suisses.mariage set "idMariage"='51262104964'
+where "idMariage" like '51262104964%';
+
+
+SELECT *
+FROM elites_suisses.mariage m 
+where m."idMariage" ~ 'base'
+limit 10;
 
 -- vue SQL
 drop view elites_suisses.v_mariage;
