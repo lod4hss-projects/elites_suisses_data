b585e9d4bc5ee95cb4cc38ea1a776ab33815190f
Stefan Kessler <stkess@protonmail.ch>
2026-08-27 18:34:21 +0200

    work on education

--- documentation/database_inspection/education_exploration.sql
+++ documentation/database_inspection/education_exploration.sql
@@ -4,28 +4,29 @@ select *
 from elites_suisses.education 
 limit 20;
 
-ALTER TABLE elites_suisses.education ADD PRIMARY KEY (zkp_edu);
+-- ALTER TABLE elites_suisses.education ADD PRIMARY KEY (zkp_edu);
 
 
 /* 
  * Multiple educational steps or degrees 
  * for the same person
- * 
  */
 
+-- number of persons and education segments per person
 select e."ID_IDENTITE", count(*) as num 
 from elites_suisses.education e 
 group by e."ID_IDENTITE" 
-order by num desc
-limit 20;
+order by num desc ;
+-- 16612 individual persons linked in the education table
+-- 8814 persons have multiple education segments (≥ 2)
 
--- number of different segmets per person
+-- number of different segmets per person aggregated
 select e."ID_IDENTITE",count(*) as num , string_agg(distinct e."Formation niveau", ',' order by e."Formation niveau")
 from elites_suisses.education e 
 group by e."ID_IDENTITE" 
 order by num desc;
 
--- distribution of counts
+-- distribution of counts of segments per person
 with tw1 as (select e."ID_IDENTITE", count(*) as num 
 from elites_suisses.education e 
 group by e."ID_IDENTITE" 
@@ -35,11 +36,7 @@ from tw1
 group by num
 order by num desc;
 
-
-
--- https://elitessuisses.unil.ch/p/52223
-
--- example of multiple training
+-- example of multiple training – * display all fields *
 select i.name_forename, e.*
 from elites_suisses.education e 
 	join elites_suisses.identite i on i.id = e."ID_IDENTITE" 
@@ -48,8 +45,7 @@ where i.id = 55864 -- 52223
 order by "ID_IDENTITE" 
 limit 10;	
 
-
--- example of multiple training
+-- example of multiple training - * display relevant fields *
 select i.name_forename, e.zkp_edu, e."ID_IDENTITE", e."Date", e."Formation niveau",
 	e."Titre_stxt", e."TITRE_Codé", e."Institution", e.id_entity, e."THÈSE_Directeur_IdIdentité" ,
 	e."Lieu", e."Canton", e."Pays" 
@@ -60,19 +56,15 @@ where i.id = 55864 -- 52223
 order by "ID_IDENTITE" 
 limit 10;	
 
-
-
-
--- example of multiple training
+-- example of multiple training - * display all fields for persons at a certain institution *
 select e."Date", i.name_forename, i.birth_year, e.*
 from elites_suisses.education e 
 	join elites_suisses.identite i on i.id = e."ID_IDENTITE" 
 -- where i.name_forename = 'Brenner, Ernst'
 where e."Institution" ~* 'Louva'
 order by "ID_IDENTITE" ;
-limit 10;	
 
-
+-- URL to check on elites suisses: https://elitessuisses.unil.ch/p/52223
 
 
 /*
@@ -104,8 +96,6 @@ where trim(lower(e."Formation niveau")) in ('doctorat', 'supérieure')
 and length(e."Date") < 5 
 --and e."THÈSE_Directeur_IdIdentité" is not null
 --limit 100;
-;
-
 
 
 -- inspect the view
@@ -115,15 +105,12 @@ from elites_suisses.v_education
 order by id_person
 limit 100;
 
-
-
 select *
 from elites_suisses.v_education 
 where id_directeur is not null
 --where id_person is null
 order by id_person
 limit 100;
-
 
 select *
 from elites_suisses.v_education 
@@ -155,25 +142,109 @@ order by number desc;
  */
 
 
+-- inspect information in the "Date" field
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+group by _date, "Date"
+order by _date;
+
+-- same inspect, ordered by length of the "Date" field
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+group by _date, "Date"
+order by length ("Date"), _date;
+
+-- The field contains:
+--		4-digit years (e.g. 1987)
+--		start and end dates (e.g. 1987-; -1987)
+--		date ranges	(e.g. 1985-1987)
+--		special characters like '?', '0' and strings (e.g. "env. 1991-1992")
+
+-- inspect dates/date ranges
 select trim(lower(e."Date")) _date, count(*) as number
 FROM elites_suisses.education e 
 --where trim(lower(e."Formation niveau")) = 'base'
-where length(e."Date") > 6
+where length(e."Date") > 1 -- > 6 / = 4 / = 1 / > 4
 group by trim(lower(e."Date")) 
 order by _date;
+-- order by number desc;
 
-order by number desc;
-
+-- inspect frequency of cases with dates/date ranges
 with tw1 as (
 select trim(lower(e."Date")) _date, count(*) as number
 FROM elites_suisses.education e 
-where length(e."Date") > 6 -- > 6  / = 4
+where length(e."Date") > 1 -- > 6  / = 4 / = 1 / > 4
 group by trim(lower(e."Date")) 
 )
 select sum(number) total
 from tw1;
+-- 18265 cases have a date or some information on dates assigned to it (length > 1)
+-- 30 cases have a not usable 1 bit of information in it (length = 1)
 
+/* Types of information available */
 
+-- a.) proper 4-digit years (4 characters, containing numbers only)
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where e."Date" ~* '^[0-9]{4}$'
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 16735 cases
+
+-- b.) proper periods (e.g. '1874-1878', 9 characters with two proper 4-digit years and '-')
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where e."Date" ~* '^[0-9]{4}-[0-9]{4}$'
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 83 cases
+
+-- c.) proper start year / end year, indicating periods (e.g. '1961-', '-1961', 5 characters with 4-digit year and '-' beforehand/after)
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where e."Date" ~* '^[0-9]{4}-$' or e."Date" ~* '^-[0-9]{4}$'
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 1327 cases
+
+-- d.) fields with proper 4-digit years and a question mark somewhere in the text (e.g. '1978?')
+--     these need to be checked and cleaned manually
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where e."Date" ~* '[0-9]{4}' and e."Date" like '%?%'
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 38 cases
+
+-- e.) no information at all (e.g. field is empty or [null])
+-- 14948 (empty) + 1078 [null] = 16026 cases
+
+-- f.) no meaningful information (e.g. field is only 1 character long, containing either values '?', '0' or '1')
+-- 30 cases
+
+-- g.) fields with usable or partly usable information
+--     these need to be checked and cleaned manually
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where length (e."Date") > 1
+and not (e."Date" ~* '^[0-9]{4}$') -- a.) proper 4-digit years
+and not (e."Date" ~* '^[0-9]{4}-[0-9]{4}$') -- b.) proper periods of 9 characters
+and not (e."Date" ~* '^[0-9]{4}-$' or e."Date" ~* '^-[0-9]{4}$') -- c.) proper start/end dates
+and not (e."Date" ~* '[0-9]{4}' and e."Date" like '%?%') -- d.) proper 4-digit years + '?'
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 82 cases
+
+-- Combined query for d.) & g.), listing all cases that need to be manually checked and cleaned
+select trim(lower(e."Date")) _date, count(*) as number, length("Date") as length
+FROM elites_suisses.education e 
+where length (e."Date") > 1
+and not (e."Date" ~* '^[0-9]{4}$') -- 4-digit years
+and not (e."Date" ~* '^[0-9]{4}-[0-9]{4}$') -- periods of 9 characters
+and not (e."Date" ~* '^[0-9]{4}-$' or e."Date" ~* '^-[0-9]{4}$') -- start/end dates
+group by _date, "Date"
+order by length ("Date"), _date;
+-- 38 + 82 = 120 cases
 
 
 /*
--- documentation/information_analysis/education.md
+++ documentation/information_analysis/education.md
@@ -3,12 +3,38 @@
 
 * [Base exploration](../database_inspection/education_exploration.sql)
 
+
+
+### Dates as assigned to singular events or periods (field: `Date`)
+
+| Content of `Date` field | Length |  Number of cases | Comments | Decision |
+|--------|--------|--------|----------|----------|
+| [null] | [null] | 1078 | [null] —> * no information * | leave as is |
+| '' | 0 | 14948 | emtpy field —> * no information * | set to [null] |
+| '?' | 1 | 26 | * no information * | set to [null] |
+| '0' | 1 | 3 | * no information * | set to [null] |
+| '1' | 1 | 1 | * no information * | set to [null] |
+| 4-digit year | 4 | 16735 | year | map to **study year** if information on study discipline is provided and/or map to year of **obtaining a study title** if information on study title is provided |
+| 4-digit year + '-' + 4-digit year | 5 | 83 | start year / end year (e.g. 1987- / -1987) | map to **study year** if information on study discipline is provided and/or map to year of **obtaining a study title** if information on study title is provided (**end year only**) |
+| date range with two 4-digit years | 9 | 1327 | period from year AAAA ... to year BBBB (e.g. 1985-1987) | map to **study year** if information on study discipline is provided and/or map to year of **obtaining a study title** if information on study title is provided (**end year only**) |
+| 4-digit year and question mark | varying | 38 | unclear but usable information (see note below table) (e.g. '1978?' could be treated as '1987' or '1987-?' as start year 1987) | to be cleaned manually; then map as above |
+| all other content | varying | 82 | varying and sometimes unclear but usable information | to be cleaned manually; then map as above or set to [null] |
+|   |   | **34321** |   |   |
+
+If a study title (`TITRE_Codé`) is assigned, we assign also a study title, If not, only a study period.
+
+What is the concept of a year/period (`Date`) in the database: study period, year of graduation/obtaining a study title? If there is information on the year assigned, we assign it to both, meaning that assuming that a study title cannot be obtained without having sort of 'studied' at this same year, we would assign the title at the same year as the study year.
+* If a date range is provided, we assign the study title to the end year (e.g. 1987 in values such as '-1987' or '1985-1987'). There should not be any information on a study title corresponding to a start date (e.g. '1987-' should have no study titles)
+* fields containing uncertain information but has a fully usable year assigned (e.g. '1987?' or '1987 ?'), we use that year; otherwise we drop the information and set the date to [null]. => maybe we could introduce a marker for these cases, meaning that there would be some information in the database which cannot be transformed into an entity)
+
+
+
 ## Table 'education' columns and mapping
 
 | Column Name | Data Type |  Comments | Mapping  | Mapping Comments |
 |-------------|-----------|------|------|------|
 | sysid | integer |   |   |   |
-| zkp_edu | character varying |   |   |   |
+| zkp_edu | character varying | primary key |   |   |
 | ID_IDENTITE | integer |   |   |   |
 | Formation niveau | character varying |   |   |   |
 | Titre_stxt | character varying |   |   |   |
